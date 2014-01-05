9394b32f7e59e57a195d0206c2f5b33992dc2141
Tony Dagati <tdagati@gmail.com>
2014-01-05 09:20:38 -0800

    added toc code

---
+++ Ch5/TableofContents.rb
@@ -0,0 +1,12 @@
+lineWidth=40
+str1='Table of Contents'
+puts str1.center lineWidth
+str2='Chapter 1:  Numbers'
+str3='page 1'
+puts str2.ljust(lineWidth/2) + str3.rjust(lineWidth/2)
+str4='Chapter 2:  Letters'
+str5='page 72'
+puts str4.ljust(lineWidth/2) + str5.rjust(lineWidth/2)
+str6='Chapter 3:  Variables'
+str7='page 118'
+puts str6.ljust(lineWidth/2) + str7.rjust(lineWidth/2)
