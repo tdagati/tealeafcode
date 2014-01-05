475593ec8f621b7ba168f075de66515818b3eab8
Tony Dagati <tdagati@gmail.com>
2014-01-05 09:33:14 -0800

    more code

---
+++ Ch6/BottlesofBeer.rb
@@ -0,0 +1,6 @@
+bottlesofbeer = 99
+
+while bottlesofbeer != 0
+puts bottlesofbeer.to_s + ' bottles of beer on the wall, ' + bottlesofbeer.to_s + ' bottles of beer'
+bottlesofbeer = bottlesofbeer - 1
+end
